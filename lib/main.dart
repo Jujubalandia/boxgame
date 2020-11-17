39647e3858c4bf8676d8d5bd6d78204842148411
brunormoura <brunormoura@gmail.com>
2020-11-16 23:58:41 -0300

    part one finished, on tap working

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
 import 'package:boxgame/box-game.dart';
+import 'package:flutter/gestures.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +12,8 @@ void main() async {
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
   BoxGame game = BoxGame();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
   runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper);
 }
