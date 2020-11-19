3a51719ed00d5fa69ac9d3343e019b402c0e64c4
brunormoura <brunormoura@gmail.com>
2020-11-18 23:00:54 -0300

    background and tap finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
-import 'package:boxgame/box-game.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flame/flame.dart';
+import 'package:boxgame/langaw-game.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +12,26 @@ void main() async {
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
-  BoxGame game = BoxGame();
+  Flame.images.loadAll(<String>[
+    'bg/backyard.png',
+    'flies/agile-fly-1.png',
+    'flies/agile-fly-2.png',
+    'flies/agile-fly-dead.png',
+    'flies/drooler-fly-1.png',
+    'flies/drooler-fly-2.png',
+    'flies/drooler-fly-dead.png',
+    'flies/house-fly-1.png',
+    'flies/house-fly-2.png',
+    'flies/house-fly-dead.png',
+    'flies/hungry-fly-1.png',
+    'flies/hungry-fly-2.png',
+    'flies/hungry-fly-dead.png',
+    'flies/macho-fly-1.png',
+    'flies/macho-fly-2.png',
+    'flies/macho-fly-dead.png',
+  ]);
+
+  LangawGame game = LangawGame();
   TapGestureRecognizer tapper = TapGestureRecognizer();
   tapper.onTapDown = game.onTapDown;
   runApp(game.widget);
