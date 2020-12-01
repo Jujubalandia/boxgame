cf965a9fa95675045c33b588ac081720a17f84e7
brunormoura <brunormoura@gmail.com>
2020-11-30 23:36:03 -0300

    fix fly class

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -1,21 +1,26 @@
 import 'dart:ui';
 import 'package:boxgame/langaw-game.dart';
+import 'package:flame/sprite.dart';
 
 class Fly {
   final LangawGame game;
   Rect flyRect;
-  Paint flyPaint;
   bool isDead = false;
   bool isOffScreen = false;
+  List<Sprite> flyingSprite;
+  Sprite deadSprite;
+  double flyingSpriteIndex = 0;
 
   Fly(this.game, double x, double y) {
     flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
-    flyPaint = Paint();
-    flyPaint.color = Color(0xff6ab04c);
   }
 
   void render(Canvas c) {
-    c.drawRect(flyRect, flyPaint);
+    if (isDead) {
+      deadSprite.renderRect(c, flyRect.inflate(2));
+    } else {
+      flyingSprite[flyingSpriteIndex.toInt()].renderRect(c, flyRect.inflate(2));
+    }
   }
 
   void update(double t) {
@@ -29,7 +34,6 @@ class Fly {
 
   void onTapDown() {
     isDead = true;
-    flyPaint.color = Color(0xffff4757);
     game.spawnFly();
   }
 }
